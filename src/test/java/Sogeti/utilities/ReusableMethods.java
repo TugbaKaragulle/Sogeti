ee1b28fcc74fcd261b8d87d2eac22cac946f72f0
TugbaKaragulle <karagulletugba@gmail.com>
2025-12-10 21:58:53 +0100

    update

--- src/test/java/Sogeti/utilities/ReusableMethods.java
+++ src/test/java/Sogeti/utilities/ReusableMethods.java
@@ -2,8 +2,6 @@ package Sogeti.utilities;
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.FluentWait;
-import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
